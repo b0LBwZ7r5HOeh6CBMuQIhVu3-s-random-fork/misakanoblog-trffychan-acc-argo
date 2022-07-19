13b93f826219a5279161c988f8f06813f20e8d79
永雏塔菲 <108621198+taffychan@users.noreply.github.com>
2022-07-19 16:38:24 +0800

    Update argo.sh

--- argo.sh
+++ argo.sh
@@ -41,7 +41,12 @@ for ((int = 0; int < ${#REGEX[@]}; int++)); do
 done
 
 [[ -z $SYSTEM ]] && red "不支持当前VPS的操作系统，请使用主流的操作系统" && exit 1
-[[ -z $(type -P curl) ]] && ${PACKAGE_UPDATE[int} && ${PACKAGE_INSTALL[int]} curl
+if [[ -z $(type -P curl) ]]; then
+	if [[ ! $SYSTEM == "CentOS" ]]; then
+		${PACKAGE_UPDATE[int]}
+	fi
+	${PACKAGE_INSTALL[int]} curl
+fi
 
 archAffix() {
 	case "$(uname -m)" in
